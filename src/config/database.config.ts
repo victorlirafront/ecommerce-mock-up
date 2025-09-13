9c85d7cb3568f9fe154f30b665af280e19abbc20
victorlirafront <victorliradeveloper@gmail.com>
2025-09-13 16:59:45 -0300

    remove non used filed

--- src/config/database.config.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('database', () => ({
-  host: process.env.HOST,
-  port: parseInt(process.env.PORT || '3306', 10),
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  name: process.env.DATABASE,
-}));
